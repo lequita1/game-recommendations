20871253be2ac49fd3457fa01c9a3708acc63880
wijii <quitalouie3@gmail.com>
2026-07-28 11:10:55 +0800

    fixed overlapping layout for GameCards

--- src/components/GameCards.jsx
+++ src/components/GameCards.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import "./GameCards.css";
 
 export default function GameCards({ game }) {
-  const yearReleased = game.released ? game.released.split("-")[0] : "N/A ";
+  const yearReleased = game.released ? game.released.split("-")[0] : "N/A";
+
   return (
-    <Link to={`/game/${game.id}`} style={{ textDecoration: "none" }}>
+    <Link to={`/game/${game.id}`} className="game-card-link">
       <div className="game-card">
         <img
           src={game.background_image || "https://via.placeholder.com/300x200"}
